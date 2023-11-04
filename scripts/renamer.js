a9c7c53a4d1c8c5f782eb02d0c1adec3ea91056f
K I N G S <144602610+twdevtools@users.noreply.github.com>
2023-11-04 08:18:19 -0300

    Update renamer.js

--- scripts/renamer.js
+++ scripts/renamer.js
@@ -1,68 +1,92 @@
-/*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--------------------------------------------> IT IS NOT ALLOWED TO CLONE THIS SCRIPT <-------------------------------------------
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/*
+ * Script Name: RENAME VILLAGES
+ * Version: v1.2
+ * Last Updated: 2023-11-04
+ * Author: K I N G S
+ * Author Contact: +55 48-98824-2773
+ * Approved Date: 2023-10-25
+*/
+
+/*--------------------------------------------------------------------------------------
+ * This script can NOT be cloned and modified without permission from the script author.
+ --------------------------------------------------------------------------------------*/
 
 if (window.location.href.includes('screen=overview_villages')) {
-  const $html = `<h3 align="center">Renamer</h3>
-    <div><table><tbody><tr><td><input id="firstbox" type="checkbox"></td><td><input id="start" type="text" placeholder="1" size="1"></td><td><input id="end" type="text" placeholder="3" size="1"></td></tr></tbody></table></div>
-    <div><table><tbody><tr><td><input id="secondbox" type="checkbox"></td><td><input id="textname" type="text" placeholder="Your text" maxlength="32"></td></tr></tbody></table></div>  
-    <div style="padding-top: 4px;"><table><tbody><tr><td><input id="rename" type="button" class="btn" value="Rename"></td><td><input id="save" type="button" class="btn" value="Save"></td></tr></tbody></table></div>
-    <div><br><small><strong>Rename Villages v1.1 by</strong><strong style="color: red"> K I N G S </strong></small></div>`;
+	const $html = `<h3 align="center">Renamer</h3>
+	<div>
+	  <input id="firstbox" type="checkbox">
+	  <input id="start" type="text" placeholder="1" size="1">
+	  <input id="end" type="text" placeholder="3" size="1">
+	</div>
+	<div>
+	  <input id="secondbox" type="checkbox">
+	  <input id="textname" type="text" placeholder="Your text here" maxlength="32">
+	</div>
+	<div style="padding-top: 8px;">
+	  <input id="rename" type="button" class="btn" value="Rename">
+	  <input id="save" type="button" class="btn" value="Save">
+	</div>
+	<div>
+	<br>
+	  <small>
+		<strong>Rename Villages v1.2 by<span style="color: red;"> K I N G S </span></strong>
+	  </small>
+	</div>`;
 
-  Dialog.show('rename', $html); let set=localStorage.getItem('set');
+	Dialog.show('rename', $html);
+	let set = localStorage.getItem('set');
 
-  if (set) {
-    set=JSON.parse(set);
-    $('#firstbox').prop('checked', set.firstbox);
-    $('#start').val(set.start);
-    $('#end').val(set.end);
-    $('#secondbox').prop('checked', set.secondbox);
-    $('#textname').val(set.textname);
-  }
+	if (set) {
+		set = JSON.parse(set);
+		$('#firstbox').prop('checked', set.firstbox);
+		$('#start').val(set.start);
+		$('#end').val(set.end);
+		$('#secondbox').prop('checked', set.secondbox);
+		$('#textname').val(set.textname);
+	}
 
-  $('#save')
-    .off('click')
-    .on('click', () => 
-    { set={
-        firstbox: $('#firstbox').prop('checked'),
-        start: $('#start').val(),
-        end: $('#end').val(),
-        secondbox: $('#secondbox').prop('checked'),
-        textname: $('#textname').val(),
-      };
+	$('#save').on('click', () => {
+		set = {
+			firstbox: $('#firstbox').prop('checked'),
+			start: $('#start').val(),
+			end: $('#end').val(),
+			secondbox: $('#secondbox').prop('checked'),
+			textname: $('#textname').val(),
+		};
 
-      localStorage.setItem('set', JSON.stringify(set));
+		localStorage.setItem('set', JSON.stringify(set));
 
-      UI.SuccessMessage('The settings have been saved successfully.');
-    });
+		UI.SuccessMessage('The settings have been saved successfully.');
+	});
 
-  $('#rename')
-    .off('click')
-    .on('click', () => 
-    { let n; let e;
+	$('#rename').on('click', () => {
+		let n, e;
 
-      if ($('#firstbox').prop('checked')) {
-        n=Number($('#start').val());
-        e=Number($('#end').val());
-      }
+		if ($('#firstbox').prop('checked')) {
+			n = Number($('#start').val());
+			e = Number($('#end').val());
+		}
 
-      const a=$('#secondbox').prop('checked')?$('#textname').val():'';
-      const t=$('.vis tr[class*="row"]').length;
+		const a = $('#secondbox').prop('checked') ? $('#textname').val() : '';
+		const t = $('.vis tr[class*="row"]').length;
 
-      Dialog.close();
+		Dialog.close();
 
-      async function start() {
-        for (let i=0;i<t;i++) {
-          $('.rename-icon')[i].click();
-          $('.vis input[type="text"]').val(
-            `${n&&e!==undefined?String(n+i).padStart(e,'0'):''} ${a}`
-          );
-          $('input[type="button"]').click();
-          UI.SuccessMessage(i+1+' of '+t);
-          await new Promise((resolve) => setTimeout(resolve, 200));
-        }
-      }; start();
-    });
-} else UI.InfoMessage('Use in views.');
-
-// javascript:$.getScript('https://dl.dropboxusercontent.com/scl/fi/0qo0xti6vufofsrkek76b/Renamer.js?rlkey=69bq7aeteq04arrgzfwwh0c6c&dl=0');
+		for (let i = 0; i < t; i++) {
+			setTimeout(function () {
+				$('.rename-icon')[i].click();
+				$('.vis input[type="text"]')
+					.first()
+					.val(
+						`${
+							n && e !== undefined
+								? String(n + i).padStart(e, '0')
+								: ''
+						} ${a}`
+					);
+				$('input[type="button"]').click();
+				UI.SuccessMessage(' Success: ' + (i + 1) + '/' + t);
+			}, i * 200);
+		}
+	});
+} else UI.InfoMessage('This script is intended for use in general views.');
