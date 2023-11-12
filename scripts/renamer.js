098fc728c981405a8018bdd82b18e7d62334dbe2
K I N G S <144602610+twdevtools@users.noreply.github.com>
2023-11-12 09:01:43 -0300

    Update renamer.js

--- scripts/renamer.js
+++ scripts/renamer.js
@@ -1,7 +1,7 @@
 /*
  * Script Name: RENAME VILLAGES
  * Version: v1.2
- * Last Updated: 2023-11-11
+ * Last Updated: 2023-11-12
  * Author: K I N G S
  * Author Contact: +55 48-98824-2773
  * Approved Date: 2023-10-25
@@ -14,23 +14,30 @@
 if (window.location.href.includes('screen=overview_villages')) {
 	const $html = `<h3 align="center">Renamer</h3>
 	<div>
-	  <input id="firstbox" type="checkbox">
-	  <input id="start" type="text" placeholder="1" size="1">
-	  <input id="end" type="text" placeholder="3" size="1">
+		<div class="info_box">
+			<div class="content" style="margin-left: 4px">
+				<b>WARNING:</b><br>
+				<b>1 -</b> Example 1, starting with 001.<br>
+				<b>2 -</b> Example 3, starting with 3 digits.
+			</div>
+		</div>
+		<input id="firstbox" type="checkbox">
+		<input id="start" type="text" placeholder="1" size="1">
+		<input id="end" type="text" placeholder="3" size="1">
 	</div>
 	<div>
-	  <input id="secondbox" type="checkbox">
-	  <input id="textname" type="text" placeholder="Your text here" maxlength="32">
+		<input id="secondbox" type="checkbox">
+		<input id="textname" type="text" placeholder="Your text here" maxlength="32">
 	</div>
 	<div style="padding-top: 8px;">
-	  <input id="rename" type="button" class="btn" value="Rename">
-	  <input id="save" type="button" class="btn" value="Save">
+		<input id="rename" type="button" class="btn" value="Rename Villages">
+		<input id="save" type="button" class="btn" value="Save Options">
 	</div>
-	<div>
 	<br>
-	  <small>
-		<strong>Rename Villages v1.2 by<span style="color: red;"> K I N G S </span></strong>
-	  </small>
+	<div>
+		<small>
+			<strong>Rename Villages v1.2 by<span style="color: red;"> K I N G S </span></strong>
+		</small>
 	</div>`;
 
 	Dialog.show('rename', $html);
@@ -59,7 +66,8 @@ if (window.location.href.includes('screen=overview_villages')) {
 		UI.SuccessMessage('The settings have been saved successfully.');
 	});
 
-	$('#rename').on('click', function () {
+	$('#rename').on('click', function (s) {
+		s.preventDefault();
 		let n, e;
 
 		if ($('#firstbox').prop('checked')) {
@@ -68,22 +76,23 @@ if (window.location.href.includes('screen=overview_villages')) {
 		}
 
 		const a = $('#secondbox').prop('checked') ? $('#textname').val() : '';
-		const t = Number(game_data.player.villages);
+		const total = game_data.player.villages;
 
 		Dialog.close();
 
-		for (let i = 0; i < t; i++) {
+		$('.rename-icon').each(function (i) {
+			let $this = this;
 			setTimeout(function () {
-				$('.rename-icon')[i].click();
+				$($this).click();
 				$('.vis input[type="text"]').val(
 					`${n && e !== undefined ? String(n + i).padStart(e, '0') : ''} ${a}`
 				);
 				$('input[type="button"]').click();
-				UI.SuccessMessage(' Success: ' + (i + 1) + '/' + t);
+				UI.SuccessMessage(' Success: ' + (i + 1) + '/' + total);
 			}, i * 200);
-		}
+		});
 	});
 } else
 	UI.InfoMessage('Redirecting...'),
 		(window.location.href =
-			game_data.link_base_pure + 'overview_villages&mode=combined');
+			game_data.link_base_pure + 'overview_villages&mode=combined&group=0');
