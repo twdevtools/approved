eef7253790db246d1e1a9a8153c2d804f7cccc1e
K I N G S <144602610+twdevtools@users.noreply.github.com>
2024-08-01 18:48:52 -0300

    Update snipe.js

--- scripts/snipe.js
+++ scripts/snipe.js
@@ -14,7 +14,7 @@ this.ScriptAPI = {
             "b575d8d37fffa782cfa3592d1cfc65da", 
             "a0bccd9315fa3e38aef93f34cd116aa9",
         ].reduce((acc, $pb, i) => (acc[lang[$pb].toLowerCase(null)] = `${i+1}`.padStart(2, '0') || !void 0) && acc, {});
-        let scriptHTML = `<div id="ScriptAPI" class="vis content-border" style="position: fixed; border-radius: 8px; top: 30%; left: 40%; width: 650px;">
+        let scriptHTML = `<div id="ScriptAPI" class="vis content-border" style="position: fixed; border-radius: 8px; top: 30%; left: 20%; width: 650px;">
             <a href="#" onclick="return $('#ScriptAPI').remove()" style="position: absolute; font-size: 20px; top: 3px; right: 10px; z-index: 1;">X</a>
             <h4 style="text-align: center; border-radius: 8px 8px 0px 0px; font-size: 16px; padding: 6px;">SNIPE CANCELAMENTO</h4>
             <style>#ScriptAPI input[type="text"] {font-size: 13pt; background: none; border: none;}#ScriptAPI th {text-align: center;}</style>
