53b87bef8f8aaf95d253ba4b93da00d538fed515
K I N G S <144602610+twdevtools@users.noreply.github.com>
2024-08-02 07:10:11 -0300

    Update snipe.js

--- scripts/snipe.js
+++ scripts/snipe.js
@@ -16,7 +16,7 @@ this.ScriptAPI = {
         ].reduce((acc, $pb, i) => (acc[lang[$pb].toLowerCase(null)] = `${i+1}`.padStart(2, '0') || !void 0) && acc, {});
         let scriptHTML = `<div id="ScriptAPI" class="vis content-border" style="position: fixed; border-radius: 8px; top: 30%; left: 20%;">
             <style>#ScriptAPI th, #ScriptAPI h4 {background-color: #202225 !important;background-image: none;font-weight: normal; text-align: center;color: #ffffdf;}#ScriptAPI input[type="text"] {font-size: 13pt; background: none; border: none;color: #ffffdf;}#ScriptAPI button {background-image: linear-gradient(#6e7178 0%, #36393f 30%, #202225 80%, black 100%);}#ScriptAPI, #ScriptAPI td {background: #36393f !important;color: #ffffdf;}#ScriptAPI a {color: #ae441c;}</style>
-            <a href="#" onclick="return $('#ScriptAPI').remove()" style="position: absolute; font-size: 20px; top: 5px; right: 10px; z-index: 1;">X</a>
+            <a href="#" onclick="return $('#ScriptAPI').remove()" style="position: absolute; font-size: 20px; top: 3px; right: 10px; z-index: 1;">X</a>
             <h4 style="border-radius: 8px 8px 0px 0px; font-size: 16px; padding: 6px;">SNIPE CANCELAMENTO</h4>
             <table id="options" width="100%">
                 <tbody>
