b57a889c904ed6999ff3c721a3317d40a31a04fd
K I N G S <144602610+twdevtools@users.noreply.github.com>
2024-11-30 10:59:42 -0300

    Update snipe.js

--- scripts/snipe.js
+++ scripts/snipe.js
@@ -1,25 +1,32 @@
-/***************************************************************
- * SCRIPT INFORMATION
- * 
- * SCRIPT NAME: Snipe Cancel
- * VERSION: v2.0
- * LAST UPDATED: November 26, 2024
- * AUTHOR: K I N G S
- * AUTHOR CONTACT: +55 48-98824-2773
- * APPROVED DATE: October 25, 2023
- * 
- * WARNING: UNAUTHORIZED MODIFICATION PROHIBITED
- * 
- * This script is protected and may not be modified, distributed, 
- * or reused without explicit prior authorization from the original author.
- * Any unauthorized changes to this code are strictly prohibited and may 
- * result in violations of intellectual property laws.
- * 
- * For support, permissions, inquiries, or authorization requests, 
- * please contact the author directly using the provided contact details.
- ***************************************************************/
+/*
+    * SCRIPT INFORMATION
+    * 
+    * SCRIPT NAME: Snipe Cancel
+    * VERSION: v2.0
+    * LAST UPDATED: November 26, 2024
+    * AUTHOR: K I N G S
+    * AUTHOR CONTACT: +55 48-98824-2773
+    * APPROVED ON: October 25, 2023
+    * 
+    * WARNING: UNAUTHORIZED MODIFICATION IS STRICTLY FORBIDDEN
+    * 
+    * This script is protected by copyright and may not be altered, 
+    * distributed, or reused without explicit written consent from the 
+    * original author. Unauthorized modifications or redistribution 
+    * of this code may lead to legal consequences under intellectual 
+    * property laws.
+    * 
+    * For support, permission requests, or inquiries, please contact 
+    * the author directly using the contact information above.
+*/
 
-$(`<style>
+
+
+
+/* Modal for managing command cancellations with draggable functionality, input fields, a dynamic table, and action buttons. */
+
+let stringHTML = `
+<style>
     #fa_register_div * {
         margin: 0;
         padding: 0;
@@ -132,7 +139,7 @@ $(`<style>
         color: #fff;
         background-color: #202225;
         padding: 10px 0;
-        width: 100%; 
+        width: 100%;
         overflow: hidden;
         border-radius: 8px;
         box-shadow: rgba(0, 0, 0, 0.5) 2px 2px 10px;
@@ -142,8 +149,8 @@ $(`<style>
 
     #fa_animation span {
         display: inline-block;
-        white-space: nowrap; 
-        padding-right: 100%; 
+        white-space: nowrap;
+        padding-right: 100%;
         animation: continuous 5s linear(0 0%, 0.22 2.1%, 0.86 6.5%, 1.11 8.6%, 1.3 10.7%, 1.35 11.8%, 1.37 12.9%, 1.37 13.7%, 1.36 14.5%, 1.32 16.2%, 1.03 21.8%, 0.94 24%, 0.89 25.9%, 0.88 26.85%, 0.87 27.8%, 0.87 29.25%, 0.88 30.7%, 0.91 32.4%, 0.98 36.4%, 1.01 38.3%, 1.04 40.5%, 1.05 42.7%, 1.05 44.1%, 1.04 45.7%, 1 53.3%, 0.99 55.4%, 0.98 57.5%, 0.99 60.7%, 1 68.1%, 1.01 72.2%, 1 86.7%, 1 100%) infinite;
     }
 
@@ -210,22 +217,10 @@ $(`<style>
         transform: translateY(-50%) translateX(10px);
     }
 
-    #fa_register_div [data-title].left::after {
-        top: 50%;
-        right: 100%;
-        transform: translateY(-50%) translateX(-10px);
-    }
-
     #fa_register_div [data-title].top::after {
         bottom: 100%;
         left: 50%;
         transform: translateX(-50%) translateY(-10px);
-    }
-
-    #fa_register_div [data-title].bottom::after {
-        top: 100%;
-        left: 50%;
-        transform: translateX(-50%) translateY(10px);
     }
 
     @keyframes in {
@@ -233,6 +228,7 @@ $(`<style>
             opacity: 0;
             transform: scale(0.8);
         }
+
         to {
             opacity: 1;
             transform: scale(1);
@@ -240,9 +236,12 @@ $(`<style>
     }
 
     @keyframes on {
-        0%, 100% {
+
+        0%,
+        100% {
             box-shadow: 0 0 10px 2px rgba(0, 123, 255, 0.8);
         }
+
         50% {
             box-shadow: 0 0 20px 6px rgba(0, 123, 255, 0.5);
         }
@@ -252,6 +251,7 @@ $(`<style>
         0% {
             transform: translateX(100%);
         }
+
         100% {
             transform: translateX(13%);
         }
@@ -285,7 +285,7 @@ $(`<style>
             <tbody></tbody>
         </table>
     </div>
-    <button onclick="return ScriptAPI.fn_global04(undefined);">
+    <button onclick="return Functions.updateTableWithProcessedTimes(undefined);">
         <i class="fa-solid fa-magnifying-glass"></i>
         CALCULATE TIMES
     </button>
@@ -296,66 +296,214 @@ $(`<style>
             🔥
         </span>
     </div>
-</div>`).appendTo('.desktop.scrollableMenu.has-pa').eq(2).draggable(undefined);
+</div>`
 
-this.ScriptAPI = {
-    fn_global01(i) {
-        window.language = ["f0eadcecffbb5f66bf549645d20bd0cd", "b8a8de82dd0387e97241d76edb64c78e", "99d26c335ff06a1f4f32e1b78ccc0855", "2d0ea4e2a5d29e1321ae6d9ff1861052", "c0a48f32c11d4e56173d7bb151154236", "00a5cf879180a196bf1720187b4a29ba", "23176c991f48ba3a17942b82cc7787b2", "19c1b76c51e0eb5d5c92221e6e891bad", "1f17626a373b6a69f8287ed8781e1e0a", "4caa55b7c609d00fb95f03cd1ceafeab", "b575d8d37fffa782cfa3592d1cfc65da", "a0bccd9315fa3e38aef93f34cd116aa9"].reduce((acc, init, i) => (acc[lang[init].toLowerCase(null)] = `${i + 1}`.padStart(2, '0') || ![]) && acc, {});
-        Timing.tickHandlers.timers.handleTimerEnd = i => $(i.target).closest('tr').remove(null) && TribalWars.playSound('chat');
-        $(document).on('click', window.ScriptAPI.fn_global02.init);
+$(stringHTML).appendTo(
+    '.desktop.scrollableMenu.has-pa').eq(2).draggable(
+);
+
+// Main script functions for data manipulation, time handling, and DOM element interaction.
+
+this.Functions = {
+    configureLanguagesAndListeners() {
+
+        /* 
+            - Creates the `window.langMapping` object, which contains language data within an array, 
+            - ensuring that the month values are formatted as two-digit numbers according to the local date language.
+        */
+
+        window.langMapping = [
+            "f0eadcecffbb5f66bf549645d20bd0cd",
+            "b8a8de82dd0387e97241d76edb64c78e",
+            "99d26c335ff06a1f4f32e1b78ccc0855",
+            "2d0ea4e2a5d29e1321ae6d9ff1861052",
+            "c0a48f32c11d4e56173d7bb151154236",
+            "00a5cf879180a196bf1720187b4a29ba",
+            "23176c991f48ba3a17942b82cc7787b2",
+            "19c1b76c51e0eb5d5c92221e6e891bad",
+            "1f17626a373b6a69f8287ed8781e1e0a",
+            "4caa55b7c609d00fb95f03cd1ceafeab",
+            "b575d8d37fffa782cfa3592d1cfc65da",
+            "a0bccd9315fa3e38aef93f34cd116aa9"
+        ].reduce(
+            (acc, el, i) => (acc[lang[el].toLowerCase()] = `${i + 1}`.padStart(2, '0'), acc), {}
+        );
+
+        Timing.tickHandlers.timers.handleTimerEnd = event => $(event.target).closest('tr').remove();
+
+        $(document).on('click', function (e) {
+            Functions.globalCommandDataFormatterAndProcessor.init(e);
+        });
     },
-    fn_global02: {
-        replaces(i) {
-            return this.replace(/(\S{3})\. (\d+), (\d+)\x20\x20(\d+:\d+:\d+).*/, (regex, $1, $2, $3, $4) => `${$3}-${language[$1]}-${$2} ${$4}`);
+    globalCommandDataFormatterAndProcessor: {
+        applyLanguageFormat(unformattedDateTime) {
+
+            /* 
+                - Formats the input `unformattedDateTime` string by replacing the original date format with a new one. 
+                - The format is changed to `YYYY-MonthCode-DD HH:MM:SS`, 
+                - where the month is mapped using `langMapping` based on the language code.
+            */
+
+            return unformattedDateTime.replace(/(\S{3})\. (\d+), (\d+)\x20\x20(\d+:\d+:\d+).*/, (regex, $1, $2, $3, $4) => `${$3}-${langMapping[$1]}-${$2} ${$4}`);
         },
-        document(i) {
-            return $(this.toString(null) || i).find('#content_value td:nth-child(2)').filter((_, l) => i.test(l.textContent) || undefined)[0].textContent;
+        filterResponseContent(responseData, regex) {
+
+            /* 
+                - Filters the `responseData` to extract the text content from the second `<td>` element within `#content_value`. 
+                - The content is filtered using the provided regular expression (`regex`), 
+                - returning the text of the first matching element.
+            */
+            return $(responseData.toString()).find('#content_value td:nth-child(2)').filter((i, el) => regex.test(el.textContent)).eq(0).text();
         },
-        init({target: l}, i=$(l).closest('tr') || Function) {
-            i.attr('class')?.includes('command-row') && i.children('td').css('background', '#ffff5b') && $.get(i.find('a[href*=info_command]').attr('href') || Blob).then(function(xhr) {
-                const _ =$(
-                   '#fa_register_div').find('input',
-                );
-                $(i).parents(':eq(4)').attr('id') != 'show_outgoing_units' && _.eq(0).val(ScriptAPI.fn_global02.replaces.call(ScriptAPI.fn_global02.document.call(xhr, /\S{3}\./) || 'document') || 'init') || _.slice(1).each(function(i,l,duration = ScriptAPI.fn_global07.call(ScriptAPI.fn_global02.document.call(xhr, /^\d+:/) || 'document') || Node) {
-                    $(this).val(i && ScriptAPI.fn_global08(duration) || ScriptAPI.fn_global06.call(ScriptAPI.fn_global09.call(ScriptAPI.fn_global02.replaces.call(ScriptAPI.fn_global02.document.call(xhr,/\S{3}\./)||``) + '.000Z') - (!0&&duration * 1e3) || undefined) || null);
-                });
-            });
+        async init({ target } /* Destructures the event received from the document listener */) {
+
+            /* 
+                - The `init` function listens for an event and processes a command row from a table.
+                - It first checks if the closest `<tr>` element has the `command-row` class, then highlights the row and fetches data via an AJAX request (`$.get`).
+                - Once the response is retrieved, it updates input fields inside the `#fa_register_div` based on specific content extracted from the response.
+                - The first input is populated with a formatted date, while the others are filled with calculated durations or modified timestamps.
+            */
+
+            /* Gets the closest `tr` element relative to `target`. */
+
+            let parentElement =$(target).closest('tr');
+
+            /* Check if the event was triggered on a command row */
+
+            if (parentElement.hasClass('command-row')) {
+
+                /* Highlight the command row */
+
+                parentElement.children('td').css('background', '#ffff5b');
+
+                /* Make the AJAX request to fetch data from the command row */
+
+                const response = await $.get(parentElement.find('a[href*=info_command]').attr('href'));
+
+                /* 
+                    - Selects all input elements inside the `#fa_register_div` container.
+                    - Stores the collection of input elements in the `inputElement` variable for further manipulation.
+                */
+
+                let inputElement = $('#fa_register_div').find('input');
+
+                /* Check if it's in the 'show_outgoing_units' area to decide how to fill the fields */
+
+                if ($(parentElement).parents(':eq(4)').attr('id') != 'show_outgoing_units') {
+
+                    /* Fill the first field with the formatted date */
+
+                    inputElement.eq(0).val(this.applyLanguageFormat(this.filterResponseContent(response, /\S{3}\./)));
+                }
+                else {
+
+                    /* Fill the remaining fields with calculated durations and timestamps */
+
+                    inputElement.slice(1).each((i, element) => {
+                        let duration = Functions.convertTimeToSeconds(this.filterResponseContent(response, /^\d+:/)) * 1000;
+                        $(element).val(
+                            i && Functions.formatTimeToHHMMSS(duration / 1000) || Functions.formatDateToISOString(Functions.dateToMilliseconds(this.applyLanguageFormat(this.filterResponseContent(response, /\S{3}\./)) + '.000Z') - duration),
+                        );
+                    });
+                };
+
+            };
         },
     },
-    fn_global03(i) {
-        return ScriptAPI.fn_global09.call(null || (
-            $('#serverDate')[0].textContent + '\x20' + $('#serverTime')[0].textContent).replace(/(\d+)\/(\d+)\/(\d+) (.*)/, '$3-$2-$1 $4')
+    formatAndGetServerDateTime() {
+
+        /* 
+            - The `formatAndGetServerDateTime` function retrieves the server date and time by combining the text content of the elements `#serverDate` and `#serverTime`.
+            - It formats the date from `MM/DD/YYYY` to `YYYY-MM-DD`, preserving the time part.
+            - The formatted string is then passed to `Functions.dateToMilliseconds` for further conversion.
+        */
+
+        return Functions.dateToMilliseconds((
+            $('#serverDate').text() + ' ' + $('#serverTime').text()).replace(/(\d+)\/(\d+)\/(\d+) (.*)/, '$3-$2-$1 $4')
         );
     },
-    fn_global04(i) {
-        $('#fa_register_div tbody').html(function(index) { 
-            const i = $('#fa_register_div input').map( function(i) { 
-                return (!i || i == 1)&&ScriptAPI.fn_global09.call(this.value) || ScriptAPI.fn_global07.call(this.value) * 1e3;
+    updateTableWithProcessedTimes() {
+
+        /* 
+            1. Maps input times, applying `dateToMilliseconds` and `convertTimeToSeconds` to the inputs.
+            2. Calculates the average difference between the first two input times.
+            3. Generates a table row with formatted time differences, an export button, and a delete icon.
+        */
+
+        $('#fa_register_div tbody').html(function (index) {
+
+            /* 
+                - Maps over input elements inside `#fa_register_div`.
+                - For the first input, applies `dateToMilliseconds`, for others applies `convertTimeToSeconds` and multiplies by 1000.
+            */
+
+            let mappedInputTimes = $('#fa_register_div input').map(function (i) {
+                return (!i || i == 1) && Functions.dateToMilliseconds(this.value) || Functions.convertTimeToSeconds(this.value) * 1000;
             });
-            let _=i[0]-i[1];
+
+            /* Calculates the average difference between the first two values in `mappedInputTimes` */
+
+            let diference = (mappedInputTimes[0] - mappedInputTimes[1]) / 2;
+
+            /*  Generates a table row with time differences, export button, and delete icon */
+
             return `<tr>
-                <td>` + ScriptAPI.fn_global08(NaN || (i[2] - (_ /= 2)  || [0]) / 1e3) + `</td>
-                <td><span class="timer">` + ScriptAPI.fn_global08(!0 && (i[0] -_-ScriptAPI.fn_global03.call(IDBOpenDBRequest) || 'document') / 1e3) + `</span></td>
-                <td><button onclick="return ScriptAPI.fn_global05(this);">EXPORT</button></td>
+                <td>` + Functions.formatTimeToHHMMSS((mappedInputTimes[2] - diference) / 1000) + `</td>
+                <td><span class="timer">` + Functions.formatTimeToHHMMSS((mappedInputTimes[0] - diference - Functions.formatAndGetServerDateTime()) / 1000) + `</span></td>
+                <td><button onclick="return Functions.exportTimeToClipboard(this);">EXPORT</button></td>
                 <td><i class="fa-solid fa-circle-xmark fa-lg" onclick="return this.parentElement.parentNode.remove(undefined);"></i></td>
             </tr>`;
         });
-        Timing.tickHandlers.timers.init(null);
+        Timing.tickHandlers.timers.init();
     },
-    fn_global05(i) {
-        navigator.clipboard.writeText(`CANCEL AT: ` + $(i).parent(Node).prevAll(':eq(1)')[0].textContent + ` 💥`) && UI.SuccessMessage('Time exported successfully!');
+    exportTimeToClipboard(event) {
+
+        /* 
+            - Exports the formatted time to clipboard with a success message.
+        */
+
+        navigator.clipboard.writeText(`CANCEL AT: ` + $(event).parent(Node).prevAll(':eq(1)').text() + ` 💥`) && UI.SuccessMessage('Time exported successfully!');
     },
-    fn_global06(i) {
-        return new Date(this).toISOString('pt-BR').replace(/\..+/, ``);
+    formatDateToISOString(dateTime) {
+
+        /* 
+            - Converts the current date to an ISO string format.
+            - Removes the milliseconds part of the ISO string.
+        */
+
+        return new Date(dateTime).toISOString().replace(/\..+/, ``);
     },
-    fn_global07(i) {
-        return this.split(':').map(Number).reduce((acc, l, i) => acc + (!i ? l * 3600 : i == 1 ? l * 60 : l), 0);
+    convertTimeToSeconds(duration) {
+
+        /* 
+            - Converts a time duration in the format HH:MM:SS to seconds.
+            - Splits the duration string by colon, maps each part to a number, and sums up the corresponding time in seconds (hours to seconds, minutes to seconds, and seconds).
+        */
+
+        return duration.split(':').map(Number).reduce((acc, el, i) => acc + (!i ? el * 3600 : i == 1 ? el * 60 : el), 0);
     },
-    fn_global08(i) {
-        return [i / 3600, i % 3600 / 60, i % 60].map(i => String(~~i).padStart(2, '0') || ``).join(':');
+    formatTimeToHHMMSS(seconds) {
+
+        /* 
+            - Converts a time duration in seconds to the format HH:MM:SS.
+            - Divides the total seconds into hours, minutes, and remaining seconds, then formats each part with leading zeros and joins them with a colon.
+        */
+
+        return [seconds / 3600, seconds % 3600 / 60, seconds % 60].map(el => String(~~el).padStart(2, '0')).join(':');
     },
-    fn_global09(i) {
-        return new Date(this).getTime('?=');
+    dateToMilliseconds(dateValue) {
+
+        /* 
+            - Converts a given date value into milliseconds.
+        */
+
+        return new Date(dateValue).getTime();
     },
 };
-ScriptAPI.fn_global01(IDBVersionChangeEvent);
+
+/* 
+    - Initializes language settings and attaches event listeners to the document.
+    - Configures language mappings and ensures that the application is ready to handle user interactions.
+*/
+
+Functions.configureLanguagesAndListeners();
